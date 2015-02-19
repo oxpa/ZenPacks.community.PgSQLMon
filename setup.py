1ce87cb930bc92e176d3e85635865531dc5c46b9
oxpa <iippolitov@gmail.com>
2015-02-19 16:09:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.PgSQLMon"
-VERSION = "2.6"
+VERSION = "2.6.1"
 AUTHOR = "Egor Puzanov"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
